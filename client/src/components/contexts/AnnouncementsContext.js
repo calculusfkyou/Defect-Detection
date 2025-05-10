c7711fde0bfbfeb834c540729730a2394f9f282e
calculusfkyou <charlie930320@gmail.com>
2025-05-10 23:37:17 +0800

    Modified README. 05/10

--- client/src/components/contexts/AnnouncementsContext.js
+++ client/src/components/contexts/AnnouncementsContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext } from 'react';
-import useAnnouncements from '../hooks/useAnnouncements';
+import useAnnouncements from '../../hooks/useAnnouncements';
 
 const AnnouncementsContext = createContext();
 
